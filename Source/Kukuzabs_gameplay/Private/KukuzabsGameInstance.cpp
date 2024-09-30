69296ad8b6291509e05c0fab36cfeafe4c5a5de6
zurH-dev <zurhgamesinc@gmail.com>
2024-09-30 13:30:57 +0300

    loading screen removing logic changed

---
+++ Source/Kukuzabs_gameplay/Private/KukuzabsGameInstance.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KukuzabsGameInstance.h"
+
+void UKukuzabsGameInstance::SetupMaxLoadedSubLevels(const int32 MaxLoadedSubLevelsToSetup)
+{
+	MaxLoadedSubLevels = MaxLoadedSubLevelsToSetup;
+}
+
+bool UKukuzabsGameInstance::CheckIfAllSubLevelsLoaded()
+{
+	LoadedSubLevelsCounter++;
+	if (LoadedSubLevelsCounter == MaxLoadedSubLevels)
+	{
+		LoadedSubLevelsCounter = 0;
+		return true;
+	}
+	return false;
+}
